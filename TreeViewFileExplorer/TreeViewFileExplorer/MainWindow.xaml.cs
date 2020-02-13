446219f022f7be71035683090447c1e9836a8fb5
Dale Stammen <dalestam@microsoft.com>
2020-02-12 17:16:04 -0800

    initial commit

---
+++ TreeViewFileExplorer/TreeViewFileExplorer/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using TreeViewFileExplorer.ShellClasses;
+
+namespace TreeViewFileExplorer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeFileSystemObjects();
+        }
+
+        private void InitializeFileSystemObjects()
+        {
+            var drives = DriveInfo.GetDrives();
+            DriveInfo.GetDrives().ToList().ForEach(drive =>
+            {
+                var fileSystemObject = new FileSystemObjectInfo(drive);
+                fileSystemObject.BeforeExplore += FileSystemObject_BeforeExplore;
+                fileSystemObject.AfterExplore += FileSystemObject_AfterExplore;
+                treeView.Items.Add(fileSystemObject);
+            });
+        }
+
+        private void FileSystemObject_AfterExplore(object sender, System.EventArgs e)
+        {
+            Cursor = Cursors.Arrow;
+        }
+
+        private void FileSystemObject_BeforeExplore(object sender, System.EventArgs e)
+        {
+            Cursor = Cursors.Wait;
+        }
+    }
+}
